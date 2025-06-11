25e257946553dcc0a7487fe22717ad1c25dde7fa
541863 <541863@student.saxion.nl>
2025-06-11 14:30:03 +0200

    Hacking the spo2 with bpm

--- src/position.cpp
+++ src/position.cpp
@@ -1,6 +1,7 @@
 #include "position.h"
 
 #include <HardwareSerial.h>
+#include <sstream>
 #include <TinyGPSPlus.h>
 
 position::position(int pin_rx, int pin_tx)
@@ -15,11 +16,14 @@ bool position::is_active(void)
 	return _active;
 }
 
-bool position::has_refreshed(void)
+void position::update(void)
 {
 	while (_hs.available() > 0)
 		_gps.encode(_hs.read());
+}
 
+bool position::has_location(void)
+{
 	return _gps.location.isValid();
 }
 
